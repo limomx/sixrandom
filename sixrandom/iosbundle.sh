e377a57e6cedb3f6bf4e1a379bfedacbb8adc1f2
fanxiaolong <fanxiaolongemail@gmail.com>
2018-04-21 10:59:44 +0800

    更新代码，有个bug

--- sixrandom/iosbundle.sh
+++ sixrandom/iosbundle.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cd /Applications/svn/sixrandom/sixrandom/
+basepath=$(cd `dirname $0`; pwd)
 react-native bundle --entry-file ./index.ios.js --bundle-output ./ios/bundle/index.ios.jsbundle --platform ios --assets-dest ./ios/bundle --dev false
