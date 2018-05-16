95e73a5c321fe99e68e97dd8fd5e23059879b0c9
fanxiaolong <fanxiaolongemail@gmail.com>
2018-05-17 00:04:32 +0800

    提交修改工具

---
+++ sixrandom/upgrade.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+brew upgrade node
+npm config set registry https://registry.npm.taobao.org --global
+npm config set disturl https://npm.taobao.org/dist --global
+npm install -g
+#npm install -g react-native-git-upgrade
+npm install -g yarn react-native-cli
+yarn config set registry https://registry.npm.taobao.org --global
+yarn config set disturl https://npm.taobao.org/dist --global
+brew upgrade watchman
+brew upgrade flow
+react-native-git-upgrade
